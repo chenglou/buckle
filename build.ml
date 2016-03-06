e1d54ad48c2b5f4d1eefedc33281b3bcafa86001
Cheng Lou <chenglou92@gmail.com>
2016-03-06 05:33:55 -0500

    Util for outputting ocamldep modules with files in topological order
    Because `ocamldep -modules -sort bla` doesn't work...

--- build.ml
+++ build.ml
@@ -29,25 +29,50 @@ let filesInAllDirs () =
     let filesInCurrDir =
       thingsInCurrDir
       |> BatArray.filter (fun a ->
-        not @@ BatSys.is_directory a &&
-        BatString.ends_with a ".ml"
-      )
+          not @@ BatSys.is_directory a &&
+          BatString.ends_with a ".ml"
+        )
     in
     let filesInSubDirs =
       thingsInCurrDir
       |> BatArray.filter (fun thing ->
           BatSys.is_directory thing &&
           (BatList.for_all
-            (fun d -> Filename.basename d <> Filename.basename thing)
-            ignoredDirs))
+             (fun d -> Filename.basename d <> Filename.basename thing)
+             ignoredDirs))
       |> BatArray.map filesInAllDirs'
     in
     BatArray.append filesInCurrDir (BatArray.concat @@ BatArray.to_list filesInSubDirs)
   in
   BatArray.to_list @@ filesInAllDirs' "./"
 
-let sameFileNameClashes () =
-  filesInAllDirs ()
+let deps () =
+  let filesString = filesInAllDirs () |> BatString.concat " " in
+  let (resultInTopologicalOrder, err, exitCode) = syscall @@ "ocamldep -sort " ^ filesString in
+  match exitCode with
+  | Unix.WEXITED 0 ->
+    let (resultModules, err, exitCode) = syscall @@ "ocamldep -modules -one-line " ^ filesString in
+    (match exitCode with
+     | Unix.WEXITED 0 ->
+       let filesInTopologicalOrder =
+         resultInTopologicalOrder
+         |> BatString.trim
+         |> BatString.nsplit ~by:" "
+       in
+       resultModules
+       |> BatString.trim
+       |> BatString.nsplit ~by:"\n"
+       |> BatList.sort (fun info1 info2 ->
+           let file1 = BatString.slice ~last:(BatString.index info1 ':') info1 in
+           let file2 = BatString.slice ~last:(BatString.index info2 ':') info2 in
+           let (index1, _) = BatList.findi (fun _ file -> file = file1) filesInTopologicalOrder in
+           let (index2, _) = BatList.findi (fun _ file -> file = file2) filesInTopologicalOrder in
+           index1 - index2)
+     | _ -> Printf.printf "Something went wrong: %s" err; [])
+  | _ -> Printf.printf "Something went wrong: %s" err; []
+
+let sameFileNameClashes files =
+  files
   |> BatList.sort (fun a b -> compare (Filename.basename a) (Filename.basename b))
   |> BatList.group_consecutive (fun a b -> Filename.basename a = Filename.basename b)
   |> BatList.filter (fun block -> BatList.length block > 1)
@@ -55,16 +80,15 @@ let sameFileNameClashes () =
 let formatClashes clashes =
   clashes
   |> BatList.map (fun block ->
-    block
-    |> BatList.map (fun x -> "- " ^ x)
-    |> BatString.concat "\n")
+      block
+      |> BatList.map (fun x -> "- " ^ x)
+      |> BatString.concat "\n")
   |> BatString.concat "\n\n"
 
 let buildCommand ~fileName =
   let builtName = (Filename.chop_extension fileName) ^ ".out" in
   (* ocamfind is temporary, just for bootstrapping, until we dogfood this and
      publish batteries and pcre and yojson on npm (hyeah right, we're screwed)*)
-  (* TODO: set env OCAMLRUNPARAM=b *)
   Printf.sprintf {|
     ocamlfind ocamlc -linkpkg -package batteries,pcre,yojson,bettererrors -g %s -o %s
   |} fileName builtName
@@ -104,45 +128,55 @@ let promptForInstall unboundModuleName =
   | Unix.WSIGNALED _ | Unix.WSTOPPED _  -> false
 
 let () =
+  (* TODO: reorder the warnings. e.g. the warning about paths with no empty
+     spaces should come first *)
   if BatArray.length Sys.argv = 1 then print_endline "Please pass the name of a file to build."
-  else
+  else (
     let fileName = Sys.argv.(1) in
     if not (Sys.file_exists fileName) then print_endline @@ fileName ^ " cannot be found."
     else
-    let clashes = sameFileNameClashes () in
-    if BatList.length clashes > 0 then
-      Printf.printf
-        "We detected files of the same name:\n%s.\n\nUnder this build system, \
-every file in your own library needs to be unique, since they correspond to \
-the name of the module you can freely refer to anywhere in the library.\n"
-        (formatClashes clashes)
-    else
-      let (successOutput, err, exitCode) = syscall @@ buildCommand ~fileName in
-      match exitCode with
-      | Unix.WEXITED 0 ->
-        (* exit code of 0 can still mean there are warnings *)
-        if err <> "" then
-          (print_endline @@ BetterErrorsMain.parseFromString ~customErrorParsers:[] err);
-        print_endline @@ BetterErrorsMain.parseFromString ~customErrorParsers:[] successOutput
-      | Unix.WEXITED 2 ->
-        let unboundModuleR = {|Error: Unbound module ([\w\.]*)|} in
-        let unboundModuleWithHintR = {|Unbound module [\w\.]*[\s\S]Hint: Did you mean \S+\?|} in
-        if Pcre.pmatch ~pat:unboundModuleWithHintR err then
-          (* there's a suggestion, probably a typo, bail and don't install dep *)
-          Printf.eprintf "%s\n" (BetterErrorsMain.parseFromString ~customErrorParsers:[] err)
-        else (
-          try
-            let foundMatch = Pcre.exec ~pat:unboundModuleR err in
-            let unboundModuleName = Pcre.get_substring foundMatch 1 in
-            let successfulInstall = promptForInstall unboundModuleName in
-            if successfulInstall then (
-              (* TODO: now what? rerun? might have other warnings and all *)
-            ) else
+      let files = filesInAllDirs () in
+      let clashes = sameFileNameClashes files in
+      if BatList.length clashes > 0 then
+        Printf.printf
+          "We detected files of the same name:\n%s.\n\nUnder this build system, \
+           every file in your own library needs to be unique, since they correspond to \
+           the name of the module you can freely refer to anywhere in the library.\n"
+          (formatClashes clashes)
+      else
+        let filesWithSpaces = BatList.filter (fun file -> BatString.exists file " ") files in
+        if BatList.length filesWithSpaces > 0 then
+          Printf.printf
+            "Some of your file/folders have spaces in them, this isn't allowed.\n%s"
+            (filesWithSpaces |> BatList.map (fun file -> "- " ^ file) |> BatString.join "\n")
+        else
+          let (successOutput, err, exitCode) = syscall (buildCommand ~fileName) in
+          match exitCode with
+          | Unix.WEXITED 0 ->
+            (* exit code of 0 can still mean there are warnings *)
+            if err <> "" then
+              (print_endline @@ BetterErrorsMain.parseFromString ~customErrorParsers:[] err);
+            print_endline @@ BetterErrorsMain.parseFromString ~customErrorParsers:[] successOutput
+          | Unix.WEXITED 2 ->
+            let unboundModuleR = {|Error: Unbound module ([\w\.]*)|} in
+            let unboundModuleWithHintR = {|Unbound module [\w\.]*[\s\S]Hint: Did you mean \S+\?|} in
+            if Pcre.pmatch ~pat:unboundModuleWithHintR err then
+              (* there's a suggestion, probably a typo, bail and don't install dep *)
               Printf.eprintf "%s\n" (BetterErrorsMain.parseFromString ~customErrorParsers:[] err)
-          with Not_found | Invalid_argument _ ->
-            (* no module error probably. Re-print out the compile error *)
-            Printf.eprintf "%s\n" (BetterErrorsMain.parseFromString ~customErrorParsers:[] err)
-        )
-      | Unix.WEXITED r ->
-        Printf.printf "Something went wrong: %s %d" err r
-      | Unix.WSIGNALED _ | Unix.WSTOPPED _  -> ()
+            else (
+              try
+                let foundMatch = Pcre.exec ~pat:unboundModuleR err in
+                let unboundModuleName = Pcre.get_substring foundMatch 1 in
+                let successfulInstall = promptForInstall unboundModuleName in
+                if successfulInstall then (
+                  (* TODO: now what? rerun? might have other warnings and all *)
+                ) else
+                  Printf.eprintf "%s\n" (BetterErrorsMain.parseFromString ~customErrorParsers:[] err)
+              with Not_found | Invalid_argument _ ->
+                (* no module error probably. Re-print out the compile error *)
+                Printf.eprintf "%s\n" (BetterErrorsMain.parseFromString ~customErrorParsers:[] err)
+            )
+          | Unix.WEXITED r ->
+            Printf.printf "Something went wrong: %s %d" err r
+          | Unix.WSIGNALED _ | Unix.WSTOPPED _  -> ()
+  )
