82b3e03d94086d0ecf60847b64af3bd179dd6094
Cheng Lou <chenglou92@gmail.com>
2016-03-12 21:27:34 -0500

    Move and rename some test files

---
+++ myMain.ml
@@ -0,0 +1,7 @@
+let a = 5
+let b = 10
+
+let () =
+  print_endline "hello";
+  print_endline Bookshop.Test.message;
+  print_endline Test.message;
--- someFolder/may.ml
+++
@@ -1,5 +0,0 @@
-let getSubstringMaybe s idx =
-  try Some (Pcre.get_substring s idx)
-  with Not_found | Invalid_argument _ -> None
-
-let () = print_endline @@ "May is loaded " ^ FirstDep.asd
